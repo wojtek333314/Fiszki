418c8a77670a60fc80288b1d89be6f71b36c9857
wojtek333314 <wojtek33314a@o2.pl>
2015-03-18 02:35:01 +0100

    first test

--- app/src/main/java/com/brotherhood/englishfiche/MenuActivity.java
+++ app/src/main/java/com/brotherhood/englishfiche/MenuActivity.java
@@ -1,10 +1,10 @@
 package com.brotherhood.englishfiche;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.support.v7.app.ActionBarActivity;
 
 
-public class MenuActivity extends ActionBarActivity {
+public class MenuActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
